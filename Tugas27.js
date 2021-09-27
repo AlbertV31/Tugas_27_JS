32d0e187ebca7fdca4cb37e595cde8af25a90cbb
Albert <albert.bagil@gmail.com>
2021-09-27 19:38:40 +0700

    Tugas 27  Javascript
    Tugas 27 Javascript untuk dikumpulkan ke Niomic

---
+++ Tugas27.js
@@ -0,0 +1,15 @@
+function gabung(){
+  var a = "No";
+  var b = " rumah saya ";
+  var c = 30;
+  var d = "A" ;
+  var e = "Jalan pangeran Tirtayasa No ";
+  var f = 70;
+
+  var hasil = a + b + String(c) + d;
+  var hasil2 = e + String(f);
+  console.log(hasil);
+  console.log(hasil2);
+}
+
+gabung()
